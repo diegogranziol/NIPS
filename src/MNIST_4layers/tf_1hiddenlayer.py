99f9b1ef225fe03f3a6007fbc81fac25f9c9fb5d
MatthewWilletts <matthewwilletts@mac.com>
2018-04-09 19:30:16 +0100

    update to run from command line

--- src/MNIST_4layers/tf_1hiddenlayer.py
+++ src/MNIST_4layers/tf_1hiddenlayer.py
@@ -24,9 +24,6 @@ from __future__ import print_function
 
 import argparse
 import sys
-import multiprocessing as mp
-import itertools
-import dill
 
 from tensorflow.examples.tutorials.mnist import input_data
 from pathos.multiprocessing import ProcessingPool as Pool
@@ -35,22 +32,14 @@ import tensorflow as tf
 import matplotlib as plt
 import numpy as np
 import math
+import os
 
 FLAGS = None
 
-
-hidden_sizes=[20]
-
-input_size=784
-
-output_size=10
-
-batch_size = 50
-
-data_dir = '/jmain01/home/JAD017/sjr01/mxw35-sjr01/mnist/input_data'
-
-save_dir = '/jmain01/home/JAD017/sjr01/mxw35-sjr01/Projects/NIPS/output/MNIST/hessians'
-
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def weight_variable(shape):
   """weight_variable generates a weight variable of a given shape."""
@@ -120,51 +109,78 @@ def jacobian_piece(vect, parameters, index):
   ddfx_i = tf.gradients(dfx_i, parameters)[0] 
   return ddfx_i
 
+def main(_):
 
-mnist = input_data.read_data_sets(data_dir, one_hot=True)
+  local_save_dir=FLAGS.save_dir+'/i'+str(input_size)+'_h'+str(hidden_size)+'_o'+str(output_size)+'_b'+str(batch_size))
+  ensure_dir(local_save_dir)
+  mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
-y_ = tf.placeholder(tf.float32, [None, 10])
-
-
-x, y, parameters = makeMLP(batch_size,input_size,hidden_sizes[0],output_size)
-
-
-# The raw formulation of cross-entropy,
-#
-#   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
-#                                 reduction_indices=[1]))
-#
-# can be numerically unstable.
-#
-# So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
-# outputs of 'y', and then average across the batch.
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  y_ = tf.placeholder(tf.float32, [None, FLAGS.output_size])
 
 
-# Get gradients of loss with repect to parameters
-dloss_dw = tf.gradients(cross_entropy, parameters)[0]
+  x, y, parameters = makeMLP(FLAGS.batch_size,FLAGS.input_size,FLAGS.hidden_size,FLAGS.output_size)
 
 
-hess = hessian_via_jacobian(cross_entropy, parameters)
-sess = tf.InteractiveSession()
-tf.global_variables_initializer().run()
-
-train_accuracy = []
-# Train
-
-# with tf.Session() as sess:
-#   sess.run(tf.global_variables_initializer())
-for i in range(20000):
-  batch = mnist.train.next_batch(batch_size)
-  if i % 100 == 0:
-    train_accuracy.append(accuracy.eval(feed_dict={x: batch[0], y_: batch[1]}))
-    print('step %d, training accuracy %g' % (i, train_accuracy[-1]))
-  if i % 1000 ==0:
-    np.save(save_dir+'/hess'+str(i)+'.npy', hess.eval(feed_dict={x: batch[0], y_: batch[1]}))
-  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+  # The raw formulation of cross-entropy,
+  #
+  #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
+  #                                 reduction_indices=[1]))
+  #
+  # can be numerically unstable.
+  #
+  # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
+  # outputs of 'y', and then average across the batch.
+  cross_entropy = tf.reduce_mean(
+      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+  train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
+  # Get gradients of loss with repect to parameters
+  dloss_dw = tf.gradients(cross_entropy, parameters)[0]
+
+
+  hess = hessian_via_jacobian(cross_entropy, parameters)
+
+  train_accuracy = []
+  test_accuracy = []
+
+  # Train
+
+  # with tf.Session() as sess:
+  #   sess.run(tf.global_variables_initializer())
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(20001):
+      batch = mnist.train.next_batch(batch_size)
+      if i % 100 == 0:
+        train_accuracy.append(accuracy.eval(feed_dict={x: batch[0], y_: batch[1]}))
+        print('step %d, training accuracy %g' % (i, train_accuracy[-1]))
+        test_accuracy.append(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+        print('step %d, test accuracy %g' % (i, test_accuracy[-1]))
+      if i % 1000 ==0:
+        np.save(local_save_dir+'/hess'+str(i)+'.npy', hess.eval(feed_dict={x: batch[0], y_: batch[1]}))
+      train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+    np.save(local_save_dir+'/accuracy.npy',np.hstack(np.arange(0,20001,100),np.array(train_accuracy),np.array(test_accuracy)))
+    print('end')
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
+                      help='Directory for storing input data')
+
+  parser.add_argument('--save_dir', type=str, default='/Projects/NIPS/output/MNIST/hessians',
+                      help='Directory for saving hessians and other data')
+
+  parser.add_argument('--input_size', type=int, default=28)
+
+  parser.add_argument('--output_size', type=int, default=10)
+
+  parser.add_argument('--hidden_size', type=int, default=50)
+
+  parser.add_argument('--batch_size', type=int, default=50)
+
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
